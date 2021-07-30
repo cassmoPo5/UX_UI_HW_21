4ef85aaf9d4005d49d5f9e86156f299c9aaa1c74
cassmoPo5 <77122000+cassmoPo5@users.noreply.github.com>
2021-07-30 18:55:24 -0400

    Relink - JS

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ $(this).attr("disabled", true);
 document.getElementById("myButton").onclick = function () {
   location.href = "'https://cassmopo5.github.io/UX_UI_HW_21/latte.html'";
 }
+$(this).attr("disabled", true);
+document.getElementById("myButton").onclick = function () {
+  location.href = "'https://cassmopo5.github.io/UX_UI_HW_21/recipes.html'";
+}
